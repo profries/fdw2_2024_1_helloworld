1c719f4baac71a4a2c6129d5c116734d1be596fb
Luis Henrique Ries <prof.ries@gmail.com>
2024-03-25 23:49:57 -0300

    Primeiro Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { TesteComponent } from './teste/teste.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyComponentComponent, TesteComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'everybody';
+  imagem = "https://angular.io/assets/images/logos/angular/shield-large.svg"
+
+  colorBlue = 'color: blue';
+  colorGreen = 'color: green';
+  colorRed = 'color: red';
+
+  colors = ['Azul', 'Verde', 'Vermelho'];
+  
+  contadorCor = 0;
+
+  trocarCor() {
+    this.contadorCor++;
+    if(this.contadorCor > 2) this.contadorCor = 0;
+  }
+  
+  setCor(cor: number){
+    this.contadorCor = cor;
+  }
 }
---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'teste-component',
+    standalone: true,
+    imports: [],
+    templateUrl: './teste.component.html'
+  })
+  
+export class TesteComponent {
+
+}
